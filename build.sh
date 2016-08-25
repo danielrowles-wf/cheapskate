f4f1393694c45bdbde4bd2bc68a9749892c6c69e
Dan Rowles <daniel.rowles@workiva.com>
2016-08-25 11:46:53 +0000

    Regenerated code in the gen-go directory with latest frugal, which supports middleware.

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-FRUGAL=drydock.workiva.org/workiva/frugal:35054
 PREFIX=github.com/danielrowles-wf/cheapskate/gen-go/
 IMPORT="github.com/Workiva/frugal/lib/go"
 TOPDIR=$PWD
 
 echo "Building parsimony"
 go get github.com/Workiva/parsimony
+
+echo "Building frugal"
+go get github.com/Workiva/frugal
 
 if [ -e ./gen-go ]; then
     echo "Remove existing gen-go directory"
@@ -24,15 +26,7 @@ echo "Fetch all required IDL files"
 $GOPATH/bin/parsimony --staging stage stingy.frugal
 
 echo "Generate GO code"
-docker run -u $UID -v "$(pwd):/data" $FRUGAL frugal --gen=go:package_prefix=$PREFIX -r stage/stingy.frugal
-
-echo "Fix missing imports"
-for bork in stingy/f_stingyservice_service.go workiva_frugal_api/f_baseservice_service.go; do
-    if ! grep $IMPORT ./gen-go/$bork; then
-        echo "Missing <$IMPORT> in ./gen-go/$bork - add manually" >&2
-        sed -i -e "s!import (!import (\n        \"$IMPORT\"!" ./gen-go/$bork;
-    fi
-done
+$GOPATH/bin/frugal --gen=go:package_prefix=$PREFIX -r stage/stingy.frugal
 
 for dir in cheapskate client; do
     echo "Building <$dir>"
